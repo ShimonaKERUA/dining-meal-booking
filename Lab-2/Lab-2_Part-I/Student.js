0b12f711f38c452be362d0288e5c72eedabdc216
Shimona (Otto) KERUA <keruashimona038@gmail.com>
2026-09-15 14:06:48 +1000

    change

--- Lab-2/Lab-2_Part-I/Student.js
+++ Lab-2/Lab-2_Part-I/Student.js
@@ -1,5 +1,5 @@
 /*
-  Program: IS305 Lab 2 - Student Identity Class
+  Program: Student Identity Class
   Student Name: Shimona KERUA
   Student ID: 241367
   Date: 17 July 2026
@@ -47,7 +47,7 @@ class Student {
     this.#lastName = value.trim();
   }
 
-  // --- Class Methods ---
+  // --- Methods ---
   getFullName() {
     return `${this.#firstName} ${this.#lastName}`;
   }
